943607200171d928f5f80d6328b5aeb6600ab8b1
E-Martinez77 <emartizzle2185@me.com>
2020-12-20 18:25:45 -0700

    Added Pseudo code and var

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// math.floor(math.raindom())
+// console.log()
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
+
+//Variable Arrays: Special Characters, Numeric Characters, lowercase characters
+//Uppercase Characters
+
+var specialCharacters= ["!","@","#","%","^","&","*","(",")","-","_","+","="];
+var numericCharacters= ["1","2","3","4","5","6","7","8","9","0"];
+var lowercaseCharacters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseCharacters= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+//Randome Generator from Array
+
+Math.floor(math.random() *uppercaseCharacters);
+console.log(uppercaseCharacters);
+//Prompt- How many characters would you like your password to contain
+
+//Alert- Click OK to confirm including special characters
+
+//Alert- Click OK to confirm including numeric characters
+
+//Alert- Click OK to confirm including lowercase characters
+
+//Click OK to confirm including uppercase characters
+
+//Alert- Must select at least one character type
