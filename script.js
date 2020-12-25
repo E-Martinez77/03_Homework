b03c1bbc1d9c34258ee0f495b819f59c0880c5e4
E-Martinez77 <emartizzle2185@me.com>
2020-12-25 16:59:47 -0700

    added pseudo code

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+  passwordText.value = password;
 
 }
 
@@ -14,25 +14,34 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+//Build array for special characters
+
+//Build array for Numbers
+
+// Build an array for uppercase letters
+
+//Build an array for lowercase letters
+
+
 // We need a generatePassword Function
 
 //How do I make an alert pop up on button click
 
 document.getElementById("generate").onclick = function () {
-    var howMany = prompt("How Many Characters would you like? (Minimum 8)");
+  var howMany = prompt("How Many Characters would you like? (Minimum 8)");
 
-    if (howMany < 8) {
-        alert("Password must be more than 8 Characters, Please try again");
-    }
-    else {
-        var specialChar = confirm("Would you like to include special characters?");
+  if (howMany < 8) {
+    alert("Password must be more than 8 Characters, Please try again");
+  }
+  else {
+    var specialChar = confirm("Would you like to include special characters?");
 
-        var includeNumber = confirm("Would you like to include numeric characters");
+    var includeNumber = confirm("Would you like to include numeric characters");
 
-        var includeNumber = confirm("Would you like to include numeric characters");
+    var includeUpper = confirm("Click OK to confirm including uppercase letters");
 
-        var includeUpper = confirm("Click OK to confirm including lowercase characters");
-    }
+    var includeLower = confirm("Click OK to confirm including lowercase letters");
+  }
 
 
 
