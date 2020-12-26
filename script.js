58f20d4901d6f246b2f1d4f882e77328b8ba66ac
E-Martinez77 <emartizzle2185@me.com>
2020-12-25 17:13:00 -0700

    added arrays

--- script.js
+++ script.js
@@ -16,12 +16,98 @@ generateBtn.addEventListener("click", writePassword);
 
 //Build array for special characters
 
+var specSymb = [
+    "!"
+    , "@"
+    , "#"
+    , "$"
+    , "%"
+    , "^"
+    , "&"
+    , "*"
+    , "("
+    , ")"
+    , "_"
+    , "-"
+  ];
+
 //Build array for Numbers
+
+var numbers = [
+    "1"
+    , "2"
+    , "3"
+    , "3"
+    , "4"
+    , "5"
+    , "6"
+    , "7"
+    , "8"
+    , "9"
+    , "0"
+  ];
 
 // Build an array for uppercase letters
 
+var upperCase = [
+    "A"
+    , "B"
+    , "C"
+    , "D"
+    , "E"
+    , "F"
+    , "G"
+    , "H"
+    , "I"
+    , "J"
+    , "K"
+    , "L"
+    , "M"
+    , "N"
+    , "O"
+    , "P"
+    , "Q"
+    , "R"
+    , "S"
+    , "T"
+    , "U"
+    , "V"
+    , "W"
+    , "X"
+    , "Y"
+    , "Z"
+  ];
+
 //Build an array for lowercase letters
 
+var lowerCase = [
+    "a"
+    , "b"
+    , "c"
+    , "d"
+    , "e"
+    , "f"
+    , "g"
+    , "h"
+    , "i"
+    , "j"
+    , "k"
+    , "l"
+    , "m"
+    , "n"
+    , "o"
+    , "p"
+    , "q"
+    , "r"
+    , "s"
+    , "t"
+    , "u"
+    , "v"
+    , "w"
+    , "x"
+    , "y"
+    , "z"
+  ];
 
 // We need a generatePassword Function
 
