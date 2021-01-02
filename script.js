a3888b02b4bad27cafe20fc7620c2486737d6e52
E-Martinez77 <emartizzle2185@me.com>
2021-01-02 12:32:33 -0700

    completed function

--- script.js
+++ script.js
@@ -14,23 +14,11 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//Build string for special characters
 
 var specSymb = "!@#$%^&*()_-";
-
-//Build string for Numbers
-
 var numbers = "1234567890";
-
-// Build string for uppercase letters
-
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-//Build string for lowercase letters
-
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-
-// We need a generatePassword Function
 
 function generatePassword() {
     var howMany = prompt("How Many Characters would you like? (Min 8, Max 128)");
@@ -41,12 +29,14 @@ function generatePassword() {
     }
     else{
         var specialCharacters = confirm("Would you like to include Special Characters?");
-        var numbers = confirm("Would you like to include numbers?");        
+        var numbers = confirm("Would you like to include numbers?");
+        var includeUpper = confirm("Would you like to include Upper Case Characters?");
+        var includeLower = confirm("Would you like to include Lower Case Characters?")        
     }
 
-    console.log(howMany);
-    console.log(specialCharacters);
-    console.log(numbers);
+    // console.log(howMany);
+    // console.log(specialCharacters);
+    // console.log(numbers);
 
     var result = "";
     var allChar = "";
@@ -57,6 +47,12 @@ function generatePassword() {
     if (numbers === true) {
         allChar = allChar + numbers;
     }
+    if (includeUpper === true) {
+        allChar = allChar + upperCase;
+    }
+    if (includeLower === true){
+        allChar = allChar + lowerCase;
+    }
 
     for (var i = 0; i < howMany; i++) {
         result += allChar.charAt(Math.floor(Math.random() * allChar.length));
@@ -66,43 +62,3 @@ function generatePassword() {
 
 }
 
-
-
-//How do I make an alert pop up on button click
-
-
-
-// document.getElementById("generate").onclick = function () {
-//     var howMany = prompt("How Many Characters would you like? (Min 8, Max 128)");
-
-
-//     //   if (howMany < 8 || howMany > 128) {
-//     //     alert("Password must be more than 8 or less than 128 Characters, Please try again");
-//     //   }
-//     //   else {
-//     //     var specialChar = confirm("Would you like to include special characters?");
-
-//     //     var includeNumber = confirm("Would you like to include numeric characters");
-
-//     //     var includeUpper = confirm("Click OK to confirm including uppercase letters");
-
-//     //     var includeLower = confirm("Click OK to confirm including lowercase letters");
-//     // }
-
-//     if (howMany > 8 || howMany < 128) {
-//     var specialChar = confirm("Would you like to include special characters?");
-
-//     var includeNumber = confirm("Would you like to include numeric characters");
-
-//     var includeUpper = confirm("Click OK to confirm including uppercase letters");
-
-//     var includeLower = confirm("Click OK to confirm including lowercase letters");
-
-//     }
-//     else {
-//     alert("Password must be more than 8 or less than 128 Characters, Please try again");
-//     }
-
-
-
-// }
