9137568ac1b4716e0bfbfa0566921e524e19fba8
E-Martinez77 <emartizzle2185@me.com>
2021-01-02 12:54:43 -0700

    added validation for character selection

--- script.js
+++ script.js
@@ -23,15 +23,15 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 function generatePassword() {
     var howMany = prompt("How Many Characters would you like? (Min 8, Max 128)");
 
-    if (howMany < 8 || howMany > 128){
+    if (howMany < 8 || howMany > 128) {
         alert("Password must be more than 8 and less than 128 Characters, Please try again.");
-        
+
     }
-    else{
+    else {
         var specialCharacters = confirm("Would you like to include Special Characters?");
         var numbers = confirm("Would you like to include numbers?");
         var includeUpper = confirm("Would you like to include Upper Case Characters?");
-        var includeLower = confirm("Would you like to include Lower Case Characters?")        
+        var includeLower = confirm("Would you like to include Lower Case Characters?")
     }
 
     // console.log(howMany);
@@ -50,8 +50,12 @@ function generatePassword() {
     if (includeUpper === true) {
         allChar = allChar + upperCase;
     }
-    if (includeLower === true){
+    if (includeLower === true) {
         allChar = allChar + lowerCase;
+    }
+    if (specialCharacters == !true && numbers == !true && includeUpper == !true && includeLower == !true){
+        alert("At least one character type must be selected");
+        return;
     }
 
     for (var i = 0; i < howMany; i++) {
